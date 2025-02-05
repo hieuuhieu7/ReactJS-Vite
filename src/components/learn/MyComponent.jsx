0109d25fe4052ae991c966f76aff7ad444bc7d5a
unknown <tunhienbikick@gmail.com>
2025-02-05 22:48:18 +0700

    Video-30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,15 @@
 
+import './style.css';
+
+// JSX: 1 parent
+// Fragment
+
 const MyComponent = () => {
     return (
-        <h4>Hi, i'm Cristiano</h4>
+        <>
+            <h4>Hi, i'm Cristiano</h4>
+            <h5 className="cr7" >CRISTIANO RONALDO</h5>
+        </>
     );
 }
 
