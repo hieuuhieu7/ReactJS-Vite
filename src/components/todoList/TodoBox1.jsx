40027f1c8382cf7bf4644427b91ae6b3cf30ff40
unknown <tunhienbikick@gmail.com>
2025-02-06 20:56:17 +0700

    Video-36

--- src/components/todoList/TodoBox1.jsx
+++ src/components/todoList/TodoBox1.jsx
@@ -2,10 +2,19 @@
 const TodoBox1 = (props) => {
     const { myFunction } = props;
     // myFunction('Hieu');
+
+    const handleClick = () => {
+        alert('Click');
+    }
+
+    const inputChange = (name) => {
+        console.log(name);
+    }
+
     return (
         <div className="todo-box-1">
-            <input type="text" placeholder="Import data" />
-            <button>ADD</button>
+            <input onChange={(event) => { inputChange(event.target.value) }} type="text" placeholder="Import data" />
+            <button onClick={handleClick} >ADD</button>
         </div>
     )
 }
