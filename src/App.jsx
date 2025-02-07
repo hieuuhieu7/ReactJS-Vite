81dee9510ce4f414b768dc655f1649838fbd5308
unknown <tunhienbikick@gmail.com>
2025-02-07 14:01:30 +0700

    Video-47

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import MyComponent from './components/learn/MyComponent'
-// import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
 import './components/todoList/todo.css';
 import reactLogo from './assets/react.svg'
 import TodoBox1 from './components/todoList/TodoBox1';
 import TodoBox2 from './components/todoList/TodoBox2';
 import { useState } from 'react';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 
 const App = () => {
-  // const [count, setCount] = useState(0)
 
   const [todoList, setTodoList] = useState([
     // { id: 1, name: 'Data - 03' },
     // { id: 2, name: 'Data - 04' },
   ])
-
-  // const clb = 'Chelsea';
-  // const info = {
-  //   name: 'Hieu',
-  //   age: 22
-  // }
 
   const addNewTodo = (name) => {
     const newTodo = {
@@ -44,44 +33,27 @@ const App = () => {
 
   return (
     <>
+      <Header />
+
       <div className="todo-container">
         <h1 className='todo-title'>TO DO LIST</h1>
-
         <TodoBox1
           addNewTodo={addNewTodo}
         />
 
-        {/* {todoList.length > 0 &&
-          <TodoBox2
-            // club={clb}
-            // information={info}
-            todoList={todoList}
-          />
-        }
-
-        {todoList.length === 0 &&
-          <div className='todo-logo'>
-            <img src={reactLogo} alt="" />
-          </div>
-        } */}
-
         {
           todoList.length > 0 ?
             <TodoBox2
-              // club={clb}
-              // information={info}
               todoList={todoList}
-              deleteTodo={deleteTodo}
-            />
-
+              deleteTodo={deleteTodo} />
             :
-
             <div className='todo-logo'>
               <img src={reactLogo} alt="" />
             </div>
         }
-
       </div>
+
+      <Footer />
     </>
   )
 }
